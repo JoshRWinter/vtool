d72b7c70e87ceb1cec0cab17eb19d16aac1b7e6e
Josh Winter <winterjoshr@gmail.com>
2016-11-03 18:38:57 -0400

    can now find the key if given long enough text (and key is size 6)

--- Main.java
+++ Main.java
@@ -1,7 +1,20 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 public class Main{
 	public static void main(String[] args){
+		Scanner consolein = new Scanner(System.in);
+		Scanner filename;
+		System.out.print("enter the filename for the ciphertext: ");
+		try{
+			filename = new Scanner(new File(/*consolein.nextLine()*/"cipher.txt"));
+		}catch(FileNotFoundException fnf){
+			System.err.println("that file doesn't exist or is not readable.\nExiting...\n");
+			return;
+		}
+
 		Vigenere v;
-		String s = "hey there";
+		String s = filename.nextLine();
 		v = new Vigenere(s);
 		System.out.println("Encrypted text: \"" + s + "\"");
 		System.out.println("Decrypted text: \"" + v.decrypt() + "\"");
--- Vigenere.java
+++ Vigenere.java
@@ -1,10 +1,12 @@
 public class Vigenere{
 	private StringBuilder ctext;
 	private StringBuilder[] alphabet; // the stripped alphabets
+	private StringBuilder key;
 	private int period;
 
 	public Vigenere(String text){
-		this.period = 3;
+		this.period = 6;
+		this.key = new StringBuilder();
 		this.ctext = new StringBuilder(text.length());
 		for(int i = 0; i < text.length(); ++i){
 			if(this.isValidChar(text.charAt(i)))
@@ -18,12 +20,40 @@ public class Vigenere{
 
 	public String decrypt(){
 		this.strip();
+		this.key.setLength(0); // clear any previous attempts at the key
 
 		for(int i = 0; i < this.period; ++i){
-			System.out.println(i + ": " + this.alphabet[i].toString());
+			System.out.println("\033[33mMost common is " + this.alphabetShift(i) + "\033[0m");
+			this.key.append((char)(this.alphabetShift(i) + 'A'));
 		}
-		return "no idea bruh";
+		System.out.println("\033[32mThe key is: \"" + key.toString() + "\"\033[0m");
+		return "no idea browski";
 	}
+
+	// this function, given the index into this.alphabet, will
+	// guess the shift that, once applied, yields plaintext
+	// for that alphabet. if it doesn't think that it is a valid
+	// alphabet, (based on letter frequencies) returns -1.
+	private int alphabetShift(int index){
+		int[] lf = new int[26];
+		for(int i = 0; i < 26; ++i)lf[i] = 0;
+
+		for(int i = 0; i < this.alphabet[index].length(); ++i){
+			lf[this.alphabet[index].charAt(i) - 'A']++;
+		}
+
+		int common = 0;
+		for(int i = 1; i < 26; ++i){
+			if(lf[i] > lf[common])
+				common = i;
+		}
+
+		if(common >= 4)
+			return common - 4; // 4 is 'E'
+		else
+			return (common - 4) + 26;
+	}
+
 
 	private void strip(){
 		this.alphabet = new StringBuilder[this.period];
