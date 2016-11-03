e2b096fb2b73361eb8593c75ffa1e928fdfd3f33
Josh Winter <winterjoshr@gmail.com>
2016-11-03 16:36:06 -0400

    can now strip alphabets

--- Vigenere.java
+++ Vigenere.java
@@ -1,22 +1,50 @@
 public class Vigenere{
 	private StringBuilder ctext;
 	private StringBuilder[] alphabet; // the stripped alphabets
+	private int period;
 
 	public Vigenere(String text){
+		this.period = 3;
 		this.ctext = new StringBuilder(text.length());
 		for(int i = 0; i < text.length(); ++i){
-			this.ctext.append(Character.toUpperCase(text.charAt(i)));
+			if(this.isValidChar(text.charAt(i)))
+				this.ctext.append(Character.toUpperCase(text.charAt(i)));
 		}
 	}
 
+	public int getPeriod(){
+		return this.period;
+	}
+
 	public String decrypt(){
-		return this.ctext.toString();
+		this.strip();
+
+		for(int i = 0; i < this.period; ++i){
+			System.out.println(i + ": " + this.alphabet[i].toString());
+		}
+		return "no idea bruh";
+	}
+
+	private void strip(){
+		this.alphabet = new StringBuilder[this.period];
+
+		// one loop for <this.period> stripped alphabets
+		for(int i = 0; i < period; ++i){
+			this.alphabet[i] = new StringBuilder();
+
+			// loop and extract the <this.period>th letter
+			for(int j = 0; j < this.ctext.length(); ++j){
+				if(j % period == i)
+					this.alphabet[i].append(this.ctext.charAt(j));
+			}
+		}
 	}
 
 	private boolean isValidChar(char c){
-		return c >= 'A' && c <= 'Z'; // only valid char if uppercase letter.
-									 // this is so the solver can ignore whitespace
-									 // and only worry about letters
+		char upper = Character.toUpperCase(c);
+		return upper >= 'A' && upper <= 'Z'; // only valid char if uppercase letter.
+											 // this is so the solver can ignore whitespace
+									 		 // and only worry about letters
 	}
 }
 
