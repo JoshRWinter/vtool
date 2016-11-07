1534f89e0ae9b61995445157d1e0acdaf1ec3fa1
Josh Winter <winterjoshr@gmail.com>
2016-11-07 16:30:28 -0500

    WordFinder.java now works pretty well

--- WordFinder.java
+++ WordFinder.java
@@ -1,45 +1,70 @@
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 public class WordFinder{
 
-	private String[] foundWords;
 	private String[] word;
 	public static final int WORD_COUNT = 10;
-	public static final int MIN_LETTERS = 3;
+	public static final int MIN_LETTERS = 4;
+	public boolean valid;
+
+	private static final int DICTIONARY_LENGTH = 86000;
 
 	public WordFinder(){
+		// load the file
 		Scanner wordsFile;
 		try{
-		   wordsFile = new Scanner(new File("americoan_english"));
+		   wordsFile = new Scanner(new File("american-english"));
 		}catch(FileNotFoundException fnf){
 			System.err.println("\033[31merror: could not find words file \"american_english\"\033[0m");
+			this.valid = false;
 			return;
 		}
+		this.valid = true;
 
-		this.word = new String[78000]; // file "american_english" contains around 77,120 words
-		this.foundWords = new String[WordFinder.WORD_COUNT];
+		// allocate the arrays
+		this.word = new String[DICTIONARY_LENGTH]; // file "american_english" contains around 86,000 words
 
+		// fill the this.word list
 		int index = 0;
 		while(wordsFile.hasNextLine()){
-			this.word[index++] = wordsFile.nextLine();
+			String s = wordsFile.nextLine();
+			if(s.length() >= WordFinder.MIN_LETTERS){
+				this.word[index++] = new String(s);
+				if(this.word[index-1].equals("english"))System.out.println("\033[1;37mFOUND IT\033[0m");
+			}
 		}
+
 		// fill the rest of 'em up with empties
-		for(int i = index; i < 78000; ++i){
+		for(int i = index; i < DICTIONARY_LENGTH; ++i){
 			this.word[i] = "";
 		}
+
 		wordsFile.close();
+		for(int i = 0; i < 20; ++i)
+			System.out.println(this.word[i]);
 	}
 
 	// pass a string, returns true if WORD_COUNT unique words of at least MIN_LETTERS are found.
 	// reliable enough for checking if something decrypted properly
 	public boolean isEnglish(String text){
+		String[] foundWords = new String[WordFinder.WORD_COUNT];
+
+		for(int i = 0; i < WordFinder.WORD_COUNT; ++i)
+			foundWords[i] = "";
+
+		if(!this.valid)
+			return false;
+
+		// loop through all the words in the words list
+		int foundWordsIndex = 0;
 		for(int i = 0; i < this.word.length; ++i){
-			if(this.word[i].length() < WordFinder.MIN_LETTERS)
-				continue;
+			// don't find words that have already been found
 			boolean conflict = false;
 			for(int j = 0; j < WordFinder.WORD_COUNT; ++j){
-				if(this.word[i].equals(this.foundWords[j])){
+				if(this.word[i].equals(foundWords[j])){
 					conflict = true;
 					break;
 				}
@@ -47,8 +72,27 @@ public class WordFinder{
 			if(conflict)
 				continue;
 
-			if(this.word[i].matches(text))
-				return true;
+			// do the regex bit
+			Pattern p = Pattern.compile(".*" + this.word[i] + ".*", Pattern.CASE_INSENSITIVE);
+			Matcher m = p.matcher(text);
+			if(m.find() && this.word[i].length() >= WordFinder.MIN_LETTERS){
+				foundWords[foundWordsIndex++] = this.word[i];
+
+				if(foundWordsIndex == WORD_COUNT){
+					// print the found words list
+					for(int x = 0; x < foundWords.length; ++x){
+						System.out.println("\033[32mthis.foundWords[" + x + "] == \"" + foundWords[x] + "\";\033[0m");
+					}
+
+					return true;
+				}
+			}
+		}
+
+		System.out.println("foundWordsIndex == " + foundWordsIndex);
+		// print the found words list
+		for(int i = 0; i < foundWords.length; ++i){
+			System.out.println("\033[31mthis.foundWords[" + i + "] == \"" + foundWords[i] + "\";\033[0m");
 		}
 		return false;
 	}
