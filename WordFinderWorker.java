f22c0862aa81127d808d7828f604603cd259c297
Josh Winter <winterjoshr@gmail.com>
2016-11-12 23:10:47 -0500

    fixed bad race condition

--- WordFinderWorker.java
+++ WordFinderWorker.java
@@ -6,34 +6,28 @@ public class WordFinderWorker extends Thread{
 	private int upper;
 	private int workerID;
 
-	public WordFinderWorker(String text, int lower, int upper, int workerID){
+	private WordFinder wf;
+
+	public WordFinderWorker(WordFinder wf, String text, int lower, int upper, int workerID){
 		this.text = text;
 		this.lower = lower;
 		this.upper = upper;
 		this.workerID = workerID;
-	}
 
-	// add words to WordFinder.foundWords
-	// return true if WordFinder.foundWordsIndex >= WordFinder.WORD_COUNT
-	private synchronized boolean addFoundWords(String fw){
-		if(WordFinder.foundWordsIndex >= WordFinder.WORD_COUNT)
-			return true;
-
-		WordFinder.foundWords[WordFinder.foundWordsIndex] = new String(fw + " (worker " + this.workerID + ")");
-		WordFinder.foundWordsIndex++;
-		return WordFinder.foundWordsIndex >= WordFinder.WORD_COUNT;
+		this.wf = wf;
 	}
 
 	public void run(){
 		// loop through all the words in the words list
 		for(int i = this.lower; i < this.upper; ++i){
 
-			if(WordFinder.word[i].length() == 0)
+			if(this.wf.dict.word[i].length() == 0){
 				return;
+			}
 
 			// look for any occurrence of <WordFinder.word[i]> in <text>
-			if(text.contains(WordFinder.word[i])){
-				if(this.addFoundWords(WordFinder.word[i])){
+			if(text.contains(this.wf.dict.word[i])){
+				if(this.wf.addFoundWord(this.wf.dict.word[i],this.workerID)){
 					return;
 				}
 			}
