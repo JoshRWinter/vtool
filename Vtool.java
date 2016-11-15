beff98bb64e554afc6b646f28540fcb55f74e3c3
Josh Winter <winterjoshr@gmail.com>
2016-11-15 02:44:20 -0500

    the main interface to this program is now through Vtool.java and not through Vigenere.java

---
+++ Vtool.java
@@ -0,0 +1,38 @@
+public class Vtool{
+	private Vigenere vig;
+	private VtoolStatus status;
+
+	public Vtool(String text, int lower, int upper){
+		this.vig = new Vigenere(this, text, lower, upper);
+		this.status = new VtoolStatus("",0,null);
+	}
+
+	public String getKey(){
+		if(this.vig.getState() != Thread.State.TERMINATED)
+			return "";
+		return this.vig.getKey();
+	};
+
+	public String[] getFoundWords(){
+		if(this.vig.getState() != Thread.State.TERMINATED)
+			return null;
+		return this.vig.getFoundWords();
+	}
+
+	public void decrypt(){
+		this.vig.start();
+	}
+
+	public synchronized VtoolStatus status(VtoolStatus newStatus){
+		if(newStatus == null){ // get status
+			return new VtoolStatus(this.status); // be sure to make a copy before returning
+		}
+		else{ // set status
+			this.status.status = newStatus.status;
+			this.status.percent = newStatus.percent;
+			this.status.decrypted = newStatus.decrypted;
+			return null;
+		}
+	}
+}
+
