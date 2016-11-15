f9dca2236c1d540faceb89558a33b9d931361d27
Josh Winter <winterjoshr@gmail.com>
2016-11-15 02:45:26 -0500

    now uses proper front interface

--- Main.java
+++ Main.java
@@ -18,26 +18,35 @@ public class Main{
 		}
 
 		String s = filename.nextLine();
-		Vigenere v = new Vigenere(s);
+		Vtool v = new Vtool(s, 1, 12);
 
 		// show the encrypted text
 		System.out.println("\033[33mEncrypted text:\033[0m\n" + s + "\n");
 
 		// decrypt it
-		String decrypted = v.decrypt(1,12);
+		v.decrypt();
 
-		if(decrypted != null){
+		// keep track of its progress
+		VtoolStatus status;
+		do{
+			try{
+				Thread.sleep(300);
+			}catch(Exception e){};
+			status = v.status(null);
+			System.out.print("\r\033[32m" + status.status + "\033[0m");
+		}while(status.decrypted == null);
+		System.out.println();
+
+		if(!status.decrypted.equals("!")){
 			// show the decrypted text
-			System.out.println("\033[33mDecrypted text:\033[0m\n\033[1m" + decrypted + "\033[0m\n\n");
+			System.out.println("\033[33mDecrypted text:\033[0m\n\033[1m" + status.decrypted + "\033[0m\n\n");
 			// show the key
 			System.out.println("\nThe key is \"\033[1;32m" + v.getKey() + "\033[0m\"");
 			// show the matched words that verified this plaintext
 			System.out.println("These are the words that verified the plain text:");
-			if(decrypted != null){
-				String[] foundWords = v.getFoundWords();
-				for(int i = 0; i < foundWords.length; ++i)
-					System.out.println(i + ": \"\033[32m" + foundWords[i] + "\033[0m\"");
-			}
+			String[] foundWords = v.getFoundWords();
+			for(int i = 0; i < foundWords.length; ++i)
+				System.out.println(i + ": \"\033[32m" + foundWords[i] + "\033[0m\"");
 		}
 		else
 			System.out.println("\033[1;31mvtool could not decrypt this message\033[0m");
